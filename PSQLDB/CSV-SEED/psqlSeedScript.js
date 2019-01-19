d40efad1b9b059f078118338dc438e7ab1e846f9
Sean <meyersean.c@gmail.com>
2019-01-18 18:15:58 -0600

    Got the stream to work

--- PSQLDB/CSV-SEED/psqlSeedScript.js
+++ PSQLDB/CSV-SEED/psqlSeedScript.js
@@ -1,46 +1,78 @@
 const fs = require("fs");
 const readFile = fs.createReadStream("./../CSV-SEED/mockData.csv");
-const writeFile = fs.createWriteStream("./testing.txt");
-const { Transform } = require("stream");
+const { Transform, Writable, Readable } = require("stream");
 const db = require("../Postgresql10mil-Controler");
 
 // CHUNK SIZE = 65536 bytes for mockData.csv
 
-const makeArrays = new Transform({
-  objectMode: true,
-  transform(chunk) {
-    let data = chunk
-      .toString()
-      .trim()
-      .split("\n");
-    for (let i = 0; i < data.length; i++) {
-      data[i] = data[i].split(",");
-    }
-    for (let i = 0; i < data.length; i++) {
-      data[i] = {
-        project_name: data[i][0],
-        creator_name: data[i][1],
-        creator_image: data[i][2],
-        blurb: data[i][3],
-        thumbnail: data[i][4],
-        full_image: data[i][5],
-        location: data[i][6],
-        catagory: data[i][7],
-        description: data[i][8]
-      };
-    }
-    console.log(JSON.stringify(data));
-    this.push(JSON.stringify(data));
-  }
+readFile.setEncoding("UTF8");
+
+readFile.on("data", chunk => {
+  readStream(chunk)
+    .pipe(changeData())
+    .pipe(dbWrite());
 });
 
-// const DbWriteStream = new Writable(
-//   {
-//     objectMode: true
-//   },
-//   (_write = function(data, encoding, callback) {
-//     db.insert(JSON.stringify(data), callback);
-//   })
-// );
+readFile.on("close", () => {
+  console.log("HERE COMES THE DATA!!!!!!!!!!!");
+});
 
-readFile.pipe(makeArrays).pipe(writeFile);
+readFile.on("error", err => {
+  console.error(err);
+});
+
+function readStream(chunk) {
+  const stream = new Readable({
+    objectMode: true,
+    highWaterMark: 65536,
+
+    read(size) {
+      console.log("reading", size);
+    }
+  });
+
+  stream.push(chunk);
+  return stream;
+}
+
+function changeData() {
+  return new Transform({
+    objectMode: true,
+    transform: (chunk, encoding, done) => {
+      console.log("Importing jibberish", chunk.length);
+      let data = chunk
+        .toString()
+        .trim()
+        .split("\n");
+      for (let i = 0; i < data.length; i++) {
+        data[i] = data[i].split(",");
+      }
+      for (let i = 0; i < data.length; i++) {
+        data[i] = {
+          project_name: data[i][0],
+          creator_name: data[i][1],
+          creator_image: data[i][2],
+          blurb: data[i][3],
+          thumbnail: data[i][4],
+          full_image: data[i][5],
+          location: data[i][6],
+          catagory: data[i][7],
+          description: data[i][8]
+        };
+      }
+      console.log("Giving write stream JS object");
+      done(null, data);
+    }
+  });
+}
+
+function dbWrite() {
+  return new Writable({
+    objectMode: true,
+    write: async (chunk, encoding, done) => {
+      console.log("inserting into DB boss!");
+      await db.insert(chunk, done);
+      done();
+    }
+  });
+}
